ac746f06fe755d247caf1f863e551e2f1027bbdc
dkrozelle <dan.rozelle@gmail.com>
2017-05-15 12:47:31 -0400

    add s3_put_s3() function to transfer existing files to s3

--- vignettes/Introduction_to_s3r.R
+++ vignettes/Introduction_to_s3r.R
@@ -1,9 +1,11 @@
 ## ---- eval=FALSE---------------------------------------------------------
-#  s3_set(bucket = "s3r-test-bucket", profile = "s3r-read-write-user")
-#  s3_ls()
-#  
-#  df <- data.frame(col1 = c(1,2,3), col2=c(2,3,4))
-#  s3_put_with(df, write.csv, "file.csv")
-#  s3_ls()
-#  
+# s3_set(bucket = "s3r-test-bucket", profile = "s3r-read-write-user")
+# s3_ls()
+# 
+# df <- data.frame(col1 = c(1,2,3), col2=c(2,3,4))
+# s3_put_with(df, write.csv, "file.csv")
+# s3_ls()
+# 
+# s3_put_s3(from = "note.txt", to = list("from_s3_put_s3", "new_filename.txt"))
+# s3_ls("from_s3_put_s3")
 
