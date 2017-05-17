01ca55caa8688a1b6d1ae947d553865647c81ae5
dkrozelle <dan.rozelle@gmail.com>
2017-05-16 21:42:45 -0400

    start vignette

--- vignettes/Introduction_to_s3r.R
+++ vignettes/Introduction_to_s3r.R
@@ -1,15 +1,33 @@
-## ---- eval=FALSE---------------------------------------------------------
-# s3_set(bucket = "s3r-test-bucket", profile = "s3r-read-write-user")
-# s3_ls()
-# 
-# df <- data.frame(col1 = c(1,2,3), col2=c(2,3,4))
-# s3_put_with(df, write.csv, "file.csv")
-# s3_ls()
-# 
-# s3_put_s3(from = "note.txt", to = list("top", "next", "third", "fourth", "filename.txt"))
-# s3_put_s3(from = "note.txt", to = list("top", "next", "third", "filename2.txt"))
-# s3_ls("top", "next", recursive = T)
-# 
-# build_uri("top", "next", "third")
+## ------------------------------------------------------------------------
+# devtools::install_github("dkrozelle/s3r")
+library(s3r)
+s3_set(bucket  = "s3r-test-bucket", 
+       profile = "s3r-read-write-user", 
+       sse     = T )
 
+## ------------------------------------------------------------------------
+s3_cd()
+
+## ------------------------------------------------------------------------
+s3_ls()
+s3_cd("top")
+s3_ls()
+
+## ------------------------------------------------------------------------
+s3_ls()
+s3_cd("top")
+s3_ls("..")
+s3_cd("..")
+s3_ls(files.only = T)
+s3_ls(dir.only = T)
+s3_cd("top", "next")
+
+
+s3_cd()
+
+## ---- eval=F-------------------------------------------------------------
+#  df <- data.frame(col1 = c(1,2,3), col2=c(2,3,4))
+#  s3_put_with(df, write.csv, "file.csv")
+#  s3_ls()
+#  
 
